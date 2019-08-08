85571318d795a88b4f8d8e7e7658401a424d6251
alevis <alevis@protonmail.com>
2019-08-07 21:29:56 -0700

    Add gulpfile and introduce royal theme.

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+const { src, dest, series, watch, parallel } = require('gulp');
+const sass = require('gulp-sass');//save
+const rename = require("gulp-rename");
+const plumber = require("gulp-plumber");//save
+const cleanCSS = require('gulp-clean-css');//save
+const autoprefixer = require("gulp-autoprefixer");//save
+const browserSync = require('browser-sync').create();
+const nodemon = require('gulp-nodemon');//save
+
+var sassOptions = {
+    errLogToConsole: true,
+    outputStyle: "expanded",
+    includePaths: "./node_modules"
+};
+
+path = {
+    scss: "./public/sass",
+    css:  "./public/css",
+    img:  "./public/images",
+    js:   "./public/scripts"
+}
+
+watched = { scss: path.scss + '/*.scss' }
+
+// Compile CSS
+function css(){    
+    return src( watched.scss )  
+        .pipe( plumber() )
+        .pipe( sass(sassOptions) )
+        .pipe( autoprefixer({ cascade: false }) )        
+        .pipe( dest(path.css) ) 
+        .pipe( rename({ suffix: ".min" }) )
+        .pipe( cleanCSS() )
+        .pipe( dest(path.css) )
+        .pipe( browserSync.reload({ stream: true }) );
+}
+
+// BrowserSync
+function bsync(done){
+    browserSync.init({
+        watch: true,        
+        files: [ watched.scss, watched.html ],
+        proxy: "localhost:3000",
+        port: "4000"
+    }); done();
+  }
+
+// Nodemon
+function nmon(cb){
+    var started = false;
+    return nodemon({
+        script: 'bin/www'
+    }).on('start', function(){
+        if(!started){
+            cb();
+            started = true;
+        }
+    });
+}
+
+// Watch files
+function watchFiles(done){
+    watch( watched.scss, css );
+    done();
+}
+
+// Start serve before broserSync task
+exports.default = parallel( series(css, watchFiles), nmon, bsync );
